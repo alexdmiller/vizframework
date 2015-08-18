6542a4a502636acca3e948038f0b171a0b318ff5
Alex Miller <alexmiller@gmail.com>
2015-08-18 16:49:18 -0700

    better flapping speed logic

--- src/com/rattyduck/viz/renderers/ButterflyBoidRenderer.java
+++ src/com/rattyduck/viz/renderers/ButterflyBoidRenderer.java
@@ -7,32 +7,37 @@ import processing.core.PGraphics;
 import processing.core.PVector;
 
 public class ButterflyBoidRenderer extends BoidRenderer {
+  private static int[] availableColors = {0xFFf2c2ff, 0xFFb3fffc, 0xffcfffb8};
   private float theta;
   private float bodySize;
   private float wingSpan;
+  private int wingColor;
   
   public ButterflyBoidRenderer(Boid b) {
     super(b);
     theta = (float) (Math.random() * Math.PI * 2);
-    bodySize = (float) (Math.random() * 15 + 1);
+    bodySize = (float) (Math.random() * 20 + 5);
     wingSpan = (float) (Math.random() * 10 + 1);
+    wingColor = availableColors[(int) Math.floor(Math.random() * availableColors.length)];
   }
 
   @Override
   public void render(PGraphics g, int millis) {
-    theta += 0.1;
-
-    g.stroke(255);
+    g.stroke(wingColor);
     g.pushMatrix();
     g.translate(boid.pos.x, boid.pos.y, boid.pos.z);
     g.rotateY((float) Math.atan2(-boid.vel.z, boid.vel.x));
     g.rotateZ((float) Math.asin(boid.vel.y / boid.vel.mag()));
     
+    if (boid.vel.y - 2 < 0) {
+      theta += (boid.vel.y - 2) / 10;
+    }
+    
     g.strokeWeight(2);
     g.line(0, 0, 0, bodySize, 0, 0);
     
     g.noStroke();
-    g.fill(150);
+    g.fill(wingColor);
     
     g.beginShape();
     g.vertex(0, 0, 0);
